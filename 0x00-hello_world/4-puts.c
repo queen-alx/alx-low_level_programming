e75a2de017a9c772e5c5fd87e84d6922c7436c3b
queen-alx <qmakara21@gmail.com>
2021-06-19 14:04:55 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  *main -entry point
  *Description: prints "\"Programming is like building a multiligual puzzel"
  *Return: 0
  */
+
 init main(void)
 {
 puts("\"Programming is like building a multiligual puzzel");
