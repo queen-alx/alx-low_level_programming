ae65790d88c7f74de44c40af262e6e8dccb4678c
queen-alx <qmakara21@gmail.com>
2021-06-20 07:55:31 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *main - prints to string
+ * main - prints to string
  *
- *Description: Prints "and that piece of art is useful..." without puts
+ * Description: Prints "and that piece of art is useful..." without puts
  *
- *Return: 1
+ * Return: 1
  */
 int main(void)
 {
 char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-long 1 = 59;
+long l = 59;
 long fd = 1;
 long syscall = 1;
 long ret = 0;
@@ -18,6 +18,6 @@ __asm__ ("syscall"
 : "a" (syscall),
 "D" (fd),
 "S" (s),
-"d" (1));
+"d" (l));
 return (1);
 }
