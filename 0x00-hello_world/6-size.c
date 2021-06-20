ede77b13a1bbbd6f3fe6eb1fd47dd7a2a84a48f6
queen-alx <qmakara21@gmail.com>
2021-06-20 00:52:58 -0700

    puts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Prints sizes of characters
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("size of an int: %zu byte(s)\n", sizeof(int));
+printf("size of long int: %zu byte(s)\n", sizeof(long int);
+printf("size of 
+printf
