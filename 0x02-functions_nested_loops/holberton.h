d42aaaba46a6cba017907f2d8be0207f569add97
queen-alx <qmakara21@gmail.com>
2021-06-22 08:08:39 -0700

    a function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+#define HOLBERTON_H
+
+/* _putchar.c */
+int _putchar(char);
+
+/*10 x alphabet */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+int _islower(int c);
+int _isalpha(int n);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
